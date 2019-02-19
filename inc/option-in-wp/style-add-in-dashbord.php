5163b89c71bc9161e5eaa2ebf71db06243f4ac85
Vahid Tahmasebi <vahid.t.1995@gmail.com>
2019-02-19 23:48:12 +0330

    New Style To site

--- inc/option-in-wp/style-add-in-dashbord.php
+++ inc/option-in-wp/style-add-in-dashbord.php
@@ -3,7 +3,7 @@
 add_action('admin_head', 'admin_custom_css');
 function admin_custom_css(){
     echo '<style>  
- 
+
   #adminmenu,#adminmenu .wp-submenu,#adminmenuback,#adminmenuwrap {width: 170px;background-color: #28353d;}
   #adminmenu .wp-submenu-head, #adminmenu a.menu-top { font-size: 14px;font-weight: 400;line-height: 18px;font-family: IRANsans!important;padding: 0;}
   #adminmenu .wp-submenu a { font-size: 13px;line-height: 18px;margin: 0;padding: 5px 0;font-family: IRANsans!important;}   
